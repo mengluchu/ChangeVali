49607ce3dd760f5f0709e15f35b539dc67e5bc0b
Meng Lu <lumeng0312@gmail.com>
2015-10-18 09:15:50 +0200

    corrected

--- ChangeVali2/R/generate confusion matrix.R
+++ ChangeVali2/R/generate confusion matrix.R
@@ -3,7 +3,7 @@
 
 #load('prodespoints00.Rdata') # mask
 #load('pdd.Rdata')           # reference data
-#groundtruth<-pdd            
+#groundtruth<-pdd
 #load('tssarar1.Rdata') # corrected st model with original array cusum
 #load('tssarar2.Rdata')#mosum
 #load('tssarar3.Rdata')#sar cusum
@@ -17,48 +17,48 @@
 #x<-c(58930:59079)
 #y<-c(48210:48359)
 #load("edivisive1.Rdata")
- 
- 
+
+
 ######### generate confusion matrix: we use pontias' producer's accuracy ###########################
 gcm<-function(changepoint, reference.sppoint=pdd,totalp=19167){
   #changepoint<-pvpoint
-  
+
   modis.mt52<-changepoint
   deterpoints<-reference.sppoint
   result<-data.frame()
   deterpoints2<-c()
-  
-  
+
+
   b9<-length(modis.mt52)
   b10<-length(unique(modis.mt52@coords))/2
-  
+
   b1<-length( deterpoints) #1898
   b2<-length(which(!is.na(over(deterpoints,modis.mt52)))) #997
-  b3<-length(unique(deterpoints[is.na(over(deterpoints,modis.mt52))]@coords))/2  
-  
-  b4<-length(unique(deterpoints[!is.na(over(deterpoints,modis.mt52))]@coords))/2  
+  b3<-length(unique(deterpoints[is.na(over(deterpoints,modis.mt52))]@coords))/2
+
+  b4<-length(unique(deterpoints[!is.na(over(deterpoints,modis.mt52))]@coords))/2
   b5<-length(unique(modis.mt52[is.na(over(modis.mt52,deterpoints))]@coords))/2 #5381 /4061
-  b6<-length(unique(modis.mt52[!is.na(over(modis.mt52,deterpoints))]@coords))/2  
+  b6<-length(unique(modis.mt52[!is.na(over(modis.mt52,deterpoints))]@coords))/2
   b7<-length(which(!is.na(over(modis.mt52,deterpoints)))) #1400/997
   b8<-length(which(is.na(over(modis.mt52,deterpoints))))
-  
-  TP = b7 
+
+  TP = b7
   FN=  b3
   FP=  b8
   P=TP+FN
   N=totalp-P
   TN=N-FP
   ALL=totalp
-  
+
   PRODUCER =TP/(TP+FP+FN)
-  
+
   result<-cbind(TP,FN,FP,TN,PRODUCER)
   names(result)<-c("TP","FN","FP","TN","producer's accuracy")
-  
+
   p1<-paste('total reference points:', b1,    'unique reference points not in result changepoints:',b3,
             'unique referece points in result changepoints',b4, 'unique reference points not in result changepoints', b5, 'unique result changepoints in reference points',b6,
             'unique total result changepoints',b10, sep='        ')
-  
+
   p2<-paste( 'TRUE POSITIVE',TP ,
              'FALSE NEGATIVE',FN ,
              'FALSE POSITIVE', FP ,
@@ -70,14 +70,14 @@ gcm<-function(changepoint, reference.sppoint=pdd,totalp=19167){
   print(p1)
   print(p2)
   return(result)
-  
+
 }
 
 ### generate confusion matrix from result array, can use a mask to mask only interest area (sppoints) and set p-value#######
 
 generatecmchange<-function( result.array,mask=prodespoints00,reference.sppoints=pdd,pv=0.05,x=c(58930:59079),y=c(48210:48359),crs=CRS("+proj=utm +zone=21 +south"),totalp=19167) #  put functions together and generate confusion matrix (filtered with a mask)
 {
-  pvpoint<- array2sp(result.array,x,y,crs=crs )
+  pvpoint<- array2sp(changearray=result.array,x=x,y=y,crs=crs )
   if(!is.null(mask))
     pvpoint<-pvpoint[mask,]
   showre<-gcm(pvpoint,reference.sppoints,totalp)
@@ -87,15 +87,15 @@ generatecmchange<-function( result.array,mask=prodespoints00,reference.sppoints=
 ### generate confusion matrix from p-value array, can use a mask to mask only interest area (sppoints) and set p-value#######
 generatecmpvalue<-function(result.array=pvaluemx ,reference.sppoints=pdd,mask=prodespoints00,pv=0.05,x=c(58930:59079),y=c(48210:48359)) #  put functions together and generate confusion matrix (filtered with a mask)
 {
-  
-  pvpoint<-array2sp(result.array,x,y,1,1,pv)
+
+  pvpoint<-pvaluearray2sp(result.array,x,y,1,1,pv)
   pvpoint<-pvpoint[mask,]
   showre<-gcm(pvpoint,reference.sppoints)
   return(showre)
 }
 
 
-generateppvalue<-function(result.array=pvaluemx ,reference.sppoints=pdd,mask=prodespoints00,pv=0.05,x=c(58930:59079),y=c(48210:48359)) 
+generateppvalue<-function(result.array=pvaluemx ,reference.sppoints=pdd,mask=prodespoints00,pv=0.05,x=c(58930:59079),y=c(48210:48359))
 {
   pvpoint<-pvaluearray2sp(result.array,x,y,1,1,pv)
   pvpoint<-pvpoint[mask,]
@@ -107,7 +107,7 @@ generatepchange<-function( result.array,mask=prodespoints00,reference.sppoints=p
   pvpoint<- array2sp(result.array,x,y,crs=crs )
   if(!is.null(mask))
     pvpoint<-pvpoint[mask,]
- 
+
   return(pvpoint)
 }
 
@@ -132,7 +132,7 @@ generatepchange<-function( result.array,mask=prodespoints00,reference.sppoints=p
 #ptssarar4<-generateppvalue(tssarar4,pv=0.05)
 #ptssarar5<-generateppvalue(tssarar5,pv=0.05)
 #ptssarar6<-generateppvalue(tssarar6,pv=0.05)
- 
+
 #ttssarar1<-generatecmpvalue(tssarar1,pdd,pv=0.05)
 #ttssarar11<-generatecmpvalue(result.array=tssarar1 ,pv=0.2)
 #ttssarar2<-generatecmpvalue(tssarar2,pdd,pv=0.05)
@@ -151,22 +151,22 @@ generatepchange<-function( result.array,mask=prodespoints00,reference.sppoints=p
 barplotcm<- function(cts,  names1=c("Ediv dst","ediv","BFAST"),n=8,title="Confusion Matrix")
 {
 par(mfrow=c(1,2))
-barplot(cts[,1:4]/22500,beside=TRUE, main=title,    
+barplot(cts[,1:4]/22500,beside=TRUE, main=title,
         legend.text=names1,
-        
+
         cex.main=1,
         font.main=1,
         col=bpy.colors(n),
         #=rainbow(8,s = 1, v = 1, start = 0.05, end = 0.35),
         #legend.text=c("MOSUM","CUSUM","AR(1) MOSUM 0.05","AR(1) CUSUM 0.05","SAR MOSUM 0.05", "SAR MOSUM 0.025","SAR CUSUM 0.05","SAR CUSUM 0.005"),
-        args.legend = list(x = "topleft", bty = "n",cex=0.8))        
+        args.legend = list(x = "topleft", bty = "n",cex=0.8))
 
- barplot(cts[,5],beside=TRUE, main='Pontius Producer\'s Accuracy',     
+ barplot(cts[,5],beside=TRUE, main='Pontius Producer\'s Accuracy',
          col=bpy.colors(n),
          font.main=1,
          cex.main=1,
         #legend.text=c("MOSUM","CUSUM","SAR MOSUM 0.05", "SAR MOSUM 0.025","SAR CUSUM 0.05","SAR CUSUM 0.005"),
-        #args.legend = list(x = "topleft", bty = "n",cex=0.6)) 
+        #args.legend = list(x = "topleft", bty = "n",cex=0.6))
  )
 }
 #barplotcm(cts, n=8)
@@ -224,14 +224,14 @@ generateRAS<-function (pva , groundtruth=groundtruth,prodespoints00=prodespoints
   groundtruth<-spTransform(groundtruth,CRS('+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs'))
   prodespoints00<-spTransform(prodespoints00,CRS('+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs'))
   pva<-spTransform(pva,CRS('+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs'))
-  
+
   FP<-pva[which(is.na(over(pva,groundtruth)))]
   FN<-groundtruth[which(is.na(over(groundtruth,pva)))]
   TP<-pva[groundtruth,]
   PRODESNE<-prodespoints00[ which(is.na(over(prodespoints00,groundtruth))),]
   BFASTNE<-prodespoints00[ which(is.na(over(prodespoints00,pva))),]
   TN<-PRODESNE[BFASTNE,]
-  
+
   TPV<-rep(1,length(data.frame(TP)[,1]))
   TNV<-rep(2,length(data.frame(TN)[,1]))
   FPV<-rep(3,length(data.frame(FP)[,1]))
@@ -245,17 +245,17 @@ generateRAS<-function (pva , groundtruth=groundtruth,prodespoints00=prodespoints
   b<-data.frame(FPdf)
   c<-data.frame(FNdf)
   d<-data.frame(TNdf)
-  
+
   names(c)<-names(a)
   names(b)<-names(a)
   names(d)<-names(a)
   abcd<-rbind(a,b,c,d)
-  
+
   coordinates(abcd)<-~x+y
   gridded(abcd)<-TRUE
-  
+
   r1<-raster(abcd)
-  
+
   raty = ratify(r1)
   rat <- levels(raty)[[1]]
   rat$class <- c('True Positive', 'True Negative', 'False Positive', 'False Negative')
@@ -275,5 +275,4 @@ generateRAS<-function (pva , groundtruth=groundtruth,prodespoints00=prodespoints
 #              , xlab="Latitude (Sinusoidal Projection)", ylab="Longitude (Sinusoidal Projection)",
 #              col.regions=cols,names.attr=c("e.divisive(monthly)", "e.divisive dst(monthly)", "BFAST (monthly)" ))
 #plot(l)
-#take all four 
- 
+#take all four
--- examples.R
+++ examples.R
@@ -1,14 +1,15 @@
 #library('rgdal')
-#library('rasterVis')
+library('rasterVis')
+
 #run the validation tools, by default using the 150 by 150 validation data and filter
 MODISCRS <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
 UTM21S <- "+proj=utm +zone=21 +south"
 
 data(prodespoints00) # mask
 data(pdd)           # reference data
-groundtruth<-pdd            
+groundtruth<-pdd
 data(tssarar2) # corrected st model with original array cusum
-#load('tssarar3.Rdata')#sar cusum
+data(tssarar3)#sar cusum
 #load('tssarar4.Rdata')#sar mosum
 #load('tssarar5.Rdata')#ar cusum
 #load('tssarar6.Rdata')#ar mosum
@@ -25,7 +26,7 @@ data(tssarar2) # corrected st model with original array cusum
 ##########################
 
 
- 
+
 #load("C:/Users/m_lu0002/Dropbox/mengluchu/bfast/SpatialPolygons.R")
 #load("C:/Users/m_lu0002/Dropbox/mengluchu/bfast/tdall2.Rdata")
 #load("C:/Users/m_lu0002/Dropbox/mengluchu/bfast/pp0.Rdata")
@@ -49,11 +50,11 @@ y<-c(48210:48359)
 
 
 ######### generate confusion matrix: we use pontias' producer's accuracy ###########################
- 
+
 ### generate confusion matrix from result array, can use a mask to mask only interest area (sppoints) and set p-value#######
- 
+
 ### generate confusion matrix from p-value array, can use a mask to mask only interest area (sppoints) and set p-value#######
-  
+
 #load('prodespoints00.Rdata') # mask
 #load('pdd.Rdata')
 #groundtruth<-pdd
@@ -67,10 +68,14 @@ y<-c(48210:48359)
 #cts<-rbind(cpe2cm, cpe1cm,cpbcm)
 #
 ### do things
+e1[is.na(e1)]<-1
+summary(l1)
+ptssarar1<-generateppvalue(result.array=e1,pv=0.05)
+ptssarar11<-generateppvalue(result.array=l1,pv=0.005)
 #ptssarar1<-generateppvalue(tssarar1,pv=0.05)
 #ptssarar11<-generateppvalue(tssarar1,pv=0.005)
 ptssarar2<-generateppvalue(tssarar2,pv=0.05)
-#ptssarar22<-generateppvalue(tssarar2,pv=0.025)
+ptssarar22<-generateppvalue(tssarar2,pv=0.025)
 #ptssarar3<-generateppvalue(tssarar3,pv=0.05)
 #ptssarar4<-generateppvalue(tssarar4,pv=0.05)
 #ptssarar5<-generateppvalue(tssarar5,pv=0.05)
@@ -78,7 +83,7 @@ ptssarar2<-generateppvalue(tssarar2,pv=0.05)
 
 #ttssarar1<-generatecmpvalue(tssarar1,pdd,pv=0.05)
 #ttssarar11<-generatecmpvalue(result.array=tssarar1 ,pv=0.2)
-#ttssarar2<-generatecmpvalue(tssarar2,pdd,pv=0.05)
+ttssarar2<-generatecmpvalue(tssarar2,pdd,pv=0.05)
 #ttssarar22<-generatecmpvalue(tssarar2,pdd,pv=0.1)
 #ttssarar3<-generatecmpvalue(tssarar3,pdd,pv=0.05)
 #ttssarar4<-generatecmpvalue(tssarar4,pdd,pv=0.05)
@@ -90,12 +95,14 @@ ptssarar2<-generateppvalue(tssarar2,pv=0.05)
 #jpeg('Pontius Producer Accuracy ts.jpg', height=6, width=10, res=400,unit="in")
 #names1<-c("OLS-MOSUM p-value: 0.05","AR(1) OLS-MOSUM pvalue: 0.05","SAR OLS-MOSUM p-value: 0.05", "SAR OLS-MOSUM p-value: 0.1","OLS-CUSUM p-value: 0.05","AR(1) OLS-CUSUM p-value: 0.05","SAR OLS-CUSUM p-value: 0.05","SAR OLS-CUSUM p-value: 0.2")
 #tex1='Confusion Matrix of Different Methods'
- 
+
 #barplotcm(cts, n=8)
-generatemapRAS1(ptssarar2,groundtruth,prodespoints00)
+generatemapRAS1(ptssarar22,groundtruth,prodespoints00)
+generatemapRAS1(ptssarar1,groundtruth,prodespoints00)
+
 #generatemapRAS1(cpe2,groundtruth,prodespoints00)
 #generatemapRAS1(cpb,groundtruth,prodespoints00)
- 
+
 #take 1 object for comparison, plot the raster
 #return the object
 #r1<-generateRAS(pva=cpe,groundtruth,prodespoints00)
@@ -109,4 +116,4 @@ generatemapRAS1(ptssarar2,groundtruth,prodespoints00)
 #              , xlab="Latitude (Sinusoidal Projection)", ylab="Longitude (Sinusoidal Projection)",
 #              col.regions=cols,names.attr=c("e.divisive(monthly)", "e.divisive dst(monthly)", "BFAST (monthly)" ))
 #plot(l)
-#take all four 
+#take all four
