98412d1530b2c9d27e070e3ed08c0955abbab758
Lu Meng <lumeng@Lus-MacBook-Air.local>
2015-10-30 13:42:06 +0100

    s

--- ChangeVali2/R/generate confusion matrix.R
+++ ChangeVali2/R/generate confusion matrix.R
@@ -95,14 +95,14 @@ generatecmpvalue<-function(result.array=pvaluemx ,reference.sppoints=pdd,mask=pr
 }
 
 
-generateppvalue<-function(result.array=pvaluemx ,reference.sppoints=pdd,mask=prodespoints00,pv=0.05,x=c(58930:59079),y=c(48210:48359),crs==CRS("+proj=utm +zone=21 +south"))
+generateppvalue<-function(result.array=pvaluemx ,reference.sppoints=pdd,mask=prodespoints00,pv=0.05,x=c(58930:59079),y=c(48210:48359),crs=NULL)
 {
   pvpoint<-pvaluearray2sp(parray=result.array,x=x,y=y,1,1,pv=pv,crs=crs)
   pvpoint<-pvpoint[mask,]
   return(pvpoint)
 }
 
-generatepchange<-function( result.array,mask=prodespoints00,reference.sppoints=pdd,pv=0.05,x=c(58930:59079),y=c(48210:48359),crs=CRS("+proj=utm +zone=21 +south")) #  put functions together and generate confusion matrix (filtered with a mask)
+generatepchange<-function( result.array,mask=prodespoints00,reference.sppoints=pdd,pv=0.05,x=c(58930:59079),y=c(48210:48359),crs=NULL) #  put functions together and generate confusion matrix (filtered with a mask)
 {
   pvpoint<- array2sp(changearray=result.array,x=x,y=y,crs=crs )
   if(!is.null(mask))
